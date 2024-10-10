28a9c50902df9d0cd32844c7bab680f0be39d5f0
NicksEdits <61987116+NicksEdits@users.noreply.github.com>
2024-10-10 16:45:12 +0200

    ok archi

--- react-native/components/nanites/Native/StyledContainer.tsx
+++ react-native/components/nanites/Native/StyledContainer.tsx
@@ -4,25 +4,24 @@ import { ViewProps } from 'react-native';
 interface StyledContainerProps extends ViewProps {
   flexDirection?: string;
   paddingRight?: string;
-  gap?: string;
+  gap?: number | string;
   paddingBottom?: string;
   marginVertical?: string;
   borderRadius?: string;
   alignItems?: string;
-  width?: string;
+  justifyContent?: string;
 }
 
 const StyledContainer = styled.View<StyledContainerProps>`
-    flex-direction: ${(props) => props.flexDirection || 'row'};
-    padding-right: ${(props) => props.paddingRight || '0'};
+    flex-direction: ${(props) => props.flexDirection || 'column'};
+    padding-right: ${(props) => props.paddingRight || '0'};  
     gap: ${(props) => props.gap || '0'};
     padding-bottom: ${(props) => props.paddingBottom || '0'};
     margin-bottom: ${(props) => props.marginVertical || '0'};
     margin-top: ${(props) => props.marginVertical || '0'};
     border-radius: ${(props) => props.borderRadius || '0'};
-    align-items: ${(props) => props.alignItems || 'start'};
-    width: ${(props) => props.width || '100%'};
-    justify-content: "center";
+    align-items: ${(props) => props.alignItems || 'center'}; 
+    justify-content: ${(props) => props.justifyContent || 'space-between'};
 `;
 
 export default StyledContainer;
