3c174139c1f4b0380c9270a2575a6486179f3b26
mathis <mathis@cookielab.fr>
2024-10-10 16:59:39 +0200

    feat(api): owner guard + update password

--- nestjs/src/users/users.controller.ts
+++ nestjs/src/users/users.controller.ts
@@ -12,6 +12,8 @@ import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { AdminGuard } from 'src/auth/guard/admin.guard'
+import { OwnerGuard } from 'src/auth/guard/owner.guard'
+import { hash } from 'crypto'
 
 @Controller('users')
 export class UsersController {
@@ -30,19 +32,22 @@ export class UsersController {
   }
 
   @Get(':id')
-  @UseGuards(AdminGuard)
+  @UseGuards(OwnerGuard)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id)
   }
 
   @Patch(':id')
-  @UseGuards(AdminGuard)
+  @UseGuards(OwnerGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      updateUserDto.password = hash('sha256', updateUserDto.password)
+    }
     return this.usersService.update(+id, updateUserDto)
   }
 
   @Delete(':id')
-  @UseGuards(AdminGuard)
+  @UseGuards(OwnerGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id)
   }
