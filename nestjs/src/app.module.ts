04e210c3dbce800c8739e913237a4a80aa208067
mathis <mathis@cookielab.fr>
2024-10-10 10:54:54 +0200

    feat(api): init api + crud User

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_ENGINE as any,
+      host: process.env.DB_HOST as any,
+      port: process.env.DB_PORT as any,
+      username: process.env.DB_USERNAME as any,
+      password: process.env.DB_PASSWORD as any,
+      database: process.env.DB_NAME as any,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nestjs/src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { User } from './entities/user.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private data: Repository<User>) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    return this.data.save(createUserDto)
+  }
+
+  findAll(): Promise<User[]> {
+    return this.data.find()
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.data.findOneBy({ id })
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const done = await this.data.update(id, { ...updateUserDto })
+    if (done.affected === 1) {
+      return this.findOne(id)
+    }
+    throw new NotFoundException()
+  }
+
+  async remove(id: number): Promise<boolean> {
+    const done = await this.data.delete(id)
+    return done.affected === 1
+  }
+}
