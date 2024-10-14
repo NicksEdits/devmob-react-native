9265b4d646501a6ad0938e34802c07369b78419e
mathis <mathis@cookielab.fr>
2024-10-14 11:41:50 +0200

    fix(front): fiw card body width

--- react-native/components/atoms/Container/Card.tsx
+++ react-native/components/atoms/Container/Card.tsx
@@ -1,6 +1,6 @@
 // Container.tsx (Un atom qui encapsule le View)
-import React from 'react';
-import { View, StyleSheet, ViewProps } from 'react-native';
+import React from "react";
+import { View, StyleSheet, ViewProps } from "react-native";
 
 const Card: React.FC<ViewProps> = ({ children, style, ...props }) => (
   <View style={[styles.container, style]} {...props}>
@@ -10,12 +10,13 @@ const Card: React.FC<ViewProps> = ({ children, style, ...props }) => (
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: '#ADD8E6',
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#ADD8E6",
     padding: 15,
     marginVertical: 10,
     borderRadius: 10,
+    width: "100%",
   },
 });
 
--- react-native/components/nanites/Native/StyledContainer.tsx
+++ react-native/components/nanites/Native/StyledContainer.tsx
@@ -1,5 +1,5 @@
-import { styled } from 'styled-components/native';
-import { ViewProps } from 'react-native';
+import { styled } from "styled-components/native";
+import { ViewProps } from "react-native";
 
 interface StyledContainerProps extends ViewProps {
   flexDirection?: string;
@@ -10,18 +10,20 @@ interface StyledContainerProps extends ViewProps {
   borderRadius?: string;
   alignItems?: string;
   justifyContent?: string;
+  width?: string;
 }
 
 const StyledContainer = styled.View<StyledContainerProps>`
-    flex-direction: ${(props) => props.flexDirection || 'column'};
-    padding-right: ${(props) => props.paddingRight || '0'};  
-    gap: ${(props) => props.gap || '0'};
-    padding-bottom: ${(props) => props.paddingBottom || '0'};
-    margin-bottom: ${(props) => props.marginVertical || '0'};
-    margin-top: ${(props) => props.marginVertical || '0'};
-    border-radius: ${(props) => props.borderRadius || '0'};
-    align-items: ${(props) => props.alignItems || 'center'}; 
-    justify-content: ${(props) => props.justifyContent || 'space-between'};
+  flex-direction: ${(props) => props.flexDirection || "column"};
+  padding-right: ${(props) => props.paddingRight || "0"};
+  width: ${(props) => props.width || "100%"};
+  gap: ${(props) => props.gap || "0"};
+  padding-bottom: ${(props) => props.paddingBottom || "0"};
+  margin-bottom: ${(props) => props.marginVertical || "0"};
+  margin-top: ${(props) => props.marginVertical || "0"};
+  border-radius: ${(props) => props.borderRadius || "0"};
+  align-items: ${(props) => props.alignItems || "center"};
+  justify-content: ${(props) => props.justifyContent || "space-between"};
 `;
 
 export default StyledContainer;
