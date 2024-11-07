24f9b0aa049f9b5614a024ee80f491066e9f0378
jfr <jeanfrancoisregis.simbare@ooshot.com>
2024-11-07 10:35:35 +0100

    fix input form

--- react-native/components/atoms/Container/PropositionForm.tsx
+++ react-native/components/atoms/Container/PropositionForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { View, TextInput, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
@@ -11,74 +11,129 @@ interface PropositionFormProps {
 }
 
 const PropositionForm: React.FC<PropositionFormProps> = ({ onSubmit, onClose, initialData }) => {
-
-const [formData, setFormData] = useState({
-    label: '',
-    username: '',
-    description: '',
-    distance: '',
+  const [formData, setFormData] = useState({
+    label: initialData?.label || '',
+    username: initialData?.username || '',
+    description: initialData?.description || '',
+    distance: initialData?.distance || '',
   });
 
-  useEffect(() => {
-    if (initialData) {
-      setFormData(initialData);
-    }
-  }, [initialData]);
+  const [formErrors, setFormErrors] = useState({
+    label: false,
+    username: false,
+    description: false,
+    distance: false,
+  });
 
   const handleChange = (field: string, value: string) => {
     setFormData(prevData => ({
       ...prevData,
       [field]: value,
     }));
+    setFormErrors(prevErrors => ({
+      ...prevErrors,
+      [field]: false,
+    }));
   };
 
   const handleSubmit = () => {
-    if(Object.values(formData).every(value => value.trim() !== '')) {
-        onSubmit(formData);
-        onClose();
+    let hasErrors = false;
+
+    if (formData.label.trim() === '') {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        label: true,
+      }));
+      hasErrors = true;
+    }
+
+    if (formData.username.trim() === '') {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        username: true,
+      }));
+      hasErrors = true;
+    }
+
+    if (formData.description.trim() === '') {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        description: true,
+      }));
+      hasErrors = true;
+    }
+
+    if (!/^\d{1,4}$/.test(formData.distance)) {
+      setFormErrors(prevErrors => ({
+        ...prevErrors,
+        distance: true,
+      }));
+      hasErrors = true;
+    }
+
+    if (!hasErrors) {
+      onSubmit(formData);
+      onClose();
     } else {
-        Alert.alert('Tous les champs sont obligatoires');
+      Alert.alert('Veuillez remplir tous les champs correctement');
     }
   };
 
   return (
     <View style={styles.container}>
-        <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-            <FontAwesomeIcon icon={faCircleXmark} size={18} color="#00afb9" />
-        </TouchableOpacity>
-        <Text.TitleCard style={styles.title}>
-            {initialData ? "Modifier la proposition" : "Faire une proposition"}
-        </Text.TitleCard>
-        <TextInput
-            style={styles.input}
-            placeholder="Label*"
-            value={formData.label}
-            onChangeText={(value) => handleChange('label', value)}
-        />
-        <TextInput
-            style={styles.input}
-            placeholder="Username*"
-            value={formData.username}
-            onChangeText={(value) => handleChange('username', value)}
-        />
-        <TextInput
-            style={styles.input}
-            placeholder="Description*"
-            value={formData.description}
-            onChangeText={(value) => handleChange('description', value)}
-        />
-        <TextInput
-            style={styles.input}
-            placeholder="Distance*"
-            value={formData.distance}
-            onChangeText={(value) => handleChange('distance', value)}
-            keyboardType="numeric"
-        />
-        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
-            <Text.DescriptionCard style={styles.submitButtonText}>
-                {initialData ? "Modifier" : "Ajouter"}
-            </Text.DescriptionCard>
-        </TouchableOpacity>
+      <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+        <FontAwesomeIcon icon={faCircleXmark} size={18} color="#00afb9" />
+      </TouchableOpacity>
+      <Text.TitleCard style={styles.title}>
+        {initialData ? "Modifier la proposition" : "Faire une proposition"}
+      </Text.TitleCard>
+      <TextInput
+        style={[
+          styles.input,
+          formErrors.label && styles.inputError,
+        ]}
+        placeholder="Label* : ex: Label"
+        value={formData.label}
+        onChangeText={(value) => handleChange('label', value)}
+      />
+      <TextInput
+        style={[
+          styles.input,
+          formErrors.username && styles.inputError,
+        ]}
+        placeholder="Username* : ex: Toto"
+        value={formData.username}
+        onChangeText={(value) => handleChange('username', value)}
+      />
+      <TextInput
+        style={[
+          styles.input,
+          formErrors.description && styles.inputError,
+        ]}
+        placeholder="Description* : ex: Lorem ipsum..."
+        value={formData.description}
+        onChangeText={(value) => handleChange('description', value)}
+      />
+      <TextInput
+        style={[
+          styles.input,
+          formErrors.distance && styles.inputError,
+        ]}
+        placeholder="Distance* : ex: 100"
+        value={formData.distance}
+        onChangeText={(value) => handleChange('distance', value)}
+        keyboardType="numeric"
+      />
+      {formErrors.distance && (
+        <Text.DescriptionCard style={styles.errorText}>
+          Distance invalide
+        </Text.DescriptionCard>
+      )}
+      <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+        <Text.DescriptionCard style={styles.submitButtonText}>
+          {initialData ? "Modifier" : "Ajouter"}
+        </Text.DescriptionCard>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -89,7 +144,6 @@ const styles = StyleSheet.create({
     padding: 20,
     borderRadius: 10,
     width: '80%',
-    // height: 300,
   },
   title: {
     fontSize: 18,
@@ -102,6 +156,10 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
+  },
+  inputError: {
+    borderColor: 'red',
+    borderWidth: 1,
   },
   submitButton: {
     backgroundColor: '#00afb9',
@@ -119,10 +177,10 @@ const styles = StyleSheet.create({
     right: 18,
     padding: 5,
     zIndex: 1,
-    // width: 24,
-    // height: 24,
-    // justifyContent: 'center',
-    // alignItems: 'center',
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 5,
   },
 });
 
