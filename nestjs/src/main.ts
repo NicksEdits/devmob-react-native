4dc29c91d3986565a984f0490ec06173ad3f62eb
NicksEdits <nicolasplanche972@gmail.com>
2024-11-27 16:10:02 +0100

    add api doc

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('The API documentation for Super Voisin')
+    .setVersion('0.4')
+    .addTag('users')
+    .addTag('posts')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);  // Accessible via /api
+
   app.enableCors({ origin: 'http://localhost:8081' })
   await app.listen(3000)
 }
